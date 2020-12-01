f788743913eea3ad27d9167052df7a311a28135b
gijo57 <pekka-1992@hotmail.com>
2020-12-01 12:32:30 +0100

    first commit day 1 challenges

---
+++ Day 1/advent.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+with open('input.txt', 'r') as f:
+    input = f.read().split('\n')
+    nums = [int(i) for i in input]
+    for x, y, z in product(nums, nums, nums):
+        if (x + y + z == 2020):
+            print(x * y * z)
+            break
