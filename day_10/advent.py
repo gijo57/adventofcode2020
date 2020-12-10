f6676790512267e23f4e3fa9c893ce3179cdcdd2
gijo57 <pekka-1992@hotmail.com>
2020-12-10 19:47:15 +0100

    day 10 answer 1

---
+++ day_10/advent.py
@@ -0,0 +1,16 @@
+differences = []
+
+with open('input.txt', 'r') as file:
+    joltages = sorted([int(joltage.strip('\n')) for joltage in file.read().split('\n')])
+    outlet_joltage = 0
+    device_joltage = max(joltages) + 3
+    joltages.insert(0, outlet_joltage)
+    joltages.append(device_joltage)
+
+    for i, j in enumerate(joltages):
+        if (i > 0):
+            differences.append(j - joltages[i-1])
+
+    answer = differences.count(1) * differences.count(3)
+
+print(answer)
