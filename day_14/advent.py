40796d4e023a3e2f8d6b2b4eede1b2e863c94938
gijo57 <pekka-1992@hotmail.com>
2020-12-14 21:24:29 +0100

    started answer 2

--- day_14/advent.py
+++ day_14/advent.py
@@ -1,22 +1,24 @@
 import re
+import numpy as np
 
 
 def mask_value(mask, value):
     mask = list(mask)
     binary = list(format(value, '036b'))
 
-    masked_value = []
+    masked_address = []
     for bits in zip(mask, binary):
         if (bits[0] == 'X'):
-            masked_value.append(bits[1])
+            masked_address.append(bits[1])
         else:
-            masked_value.append(bits[0])
-    masked_value = int(''.join(masked_value), 2)
-    return(masked_value)
+            masked_address.append(bits[0])
+    masked_address = int(''.join(masked_address), 2)
+    return(masked_address)
 
 
 with open('input.txt') as file:
     memory = {}
+    mask = ''
     for line in file:
         if ('mask' in line):
             mask = line.split(" = ")[1].strip('\n')
@@ -26,4 +28,26 @@ with open('input.txt') as file:
             memory[address] = mask_value(mask, value)
 
     answer = sum(memory.values())
-    print(answer)
+    #print(answer)
+
+
+def mask_address(mask, address):
+    mask = list(mask)
+    binary = list(format(address, '036b'))
+    masked_address = []
+
+    for bits in zip(mask, binary):
+        if (bits[0] == '0'):
+            masked_address.append(bits[1])
+        elif(bits[0] == '1'):
+            masked_address.append(bits[0])
+        else:
+            masked_address.append('X')
+
+    masked_addresses = []
+    floating_bit_indices = [i for i, b in enumerate(masked_address) if b == 'X']
+    
+    print(masked_addresses)
+
+
+print(mask_address('000000000000000000000000000000X1001X', 42))
