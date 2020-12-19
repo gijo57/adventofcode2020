bb543a1eb6d2c93a2ad7c77fea1f223db4500ba2
gijo57 <pekka-1992@hotmail.com>
2020-12-19 12:53:14 +0200

    day 14 answer 2

--- day_14/advent.py
+++ day_14/advent.py
@@ -1,5 +1,13 @@
+from os import replace
 import re
-import numpy as np
+from itertools import product
+
+
+def replace_floating_bits(address, indices, combination):
+    masked_address = address
+    for i, j in enumerate(indices):
+        masked_address[j] = str(combination[i])
+    return ''.join(masked_address)
 
 
 def mask_value(mask, value):
@@ -13,27 +21,12 @@ def mask_value(mask, value):
         else:
             masked_address.append(bits[0])
     masked_address = int(''.join(masked_address), 2)
-    return(masked_address)
-
-
-with open('input.txt') as file:
-    memory = {}
-    mask = ''
-    for line in file:
-        if ('mask' in line):
-            mask = line.split(" = ")[1].strip('\n')
-        else:
-            address = re.search('\d+', line.split(' = ')[0])[0]
-            value = int(line.split(" = ")[1].strip('\n'))
-            memory[address] = mask_value(mask, value)
-
-    answer = sum(memory.values())
-    #print(answer)
+    return masked_address
 
 
 def mask_address(mask, address):
     mask = list(mask)
-    binary = list(format(address, '036b'))
+    binary = list(format(int(address), '036b'))
     masked_address = []
 
     for bits in zip(mask, binary):
@@ -46,8 +39,30 @@ def mask_address(mask, address):
 
     masked_addresses = []
     floating_bit_indices = [i for i, b in enumerate(masked_address) if b == 'X']
-    
-    print(masked_addresses)
+    floating_bit_combinations = list(product([0, 1], repeat=len(floating_bit_indices)))
+
+    for c in floating_bit_combinations:
+        masked_addresses.append(replace_floating_bits(masked_address, floating_bit_indices, c))
+
+    return [int(a, 2) for a in masked_addresses]
 
 
-print(mask_address('000000000000000000000000000000X1001X', 42))
+with open('input.txt') as file:
+    memory = {}
+    memory2 = {}
+    mask = ''
+    for line in file:
+        if ('mask' in line):
+            mask = line.split(" = ")[1].strip('\n')
+        else:
+            address = re.search('\d+', line.split(' = ')[0])[0]
+            value = int(line.split(" = ")[1].strip('\n'))
+            memory[address] = mask_value(mask, value)
+            addresses = mask_address(mask, address)
+
+            for a in addresses:
+                memory2[a] = value
+
+answer = sum(memory.values())
+answer2 = sum(memory2.values())
+print(answer, answer2)
